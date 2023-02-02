0d8f596047ff43628b33d807fbe7667c560205a6
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-02-01 20:38:11 -0500

    commit tree aebd987c2f5eef63b2e34387bb958740a148f43b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Erkut Cakmak",
+    streetAddress: "120 Parliament",
+};
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const newEmployee = {...employee};
+    newEmployee.name = "Sam";
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(newEmployee, name, streetAddress) {
+    newEmployee.streetAddress = "12 Broadway";
+    return newEmployee;
+};
+
+function deleteFromEmployeeByKey(newEmployee, name) {
+    const deletedEmployee = {...newEmployee};
+    delete deletedEmployee.name;
+    return deletedEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(newEmployee, name) {
+    delete newEmployee.name;
+    return newEmployee;
+}
